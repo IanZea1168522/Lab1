5f46472999e9c895d76e79229db548babe54bfe0
IAN ZEA CUYUN <izea@correo.url.edu.gt>
2023-09-05 17:12:45 -0600

    Creación del modelo "aspirante"

---
+++ Laboratorio1_Estructuras2/Laboratorio1_Estructuras2/Controllers/AspiranteController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Laboratorio1_Estructuras2.Controllers
+{
+    public class AspiranteController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
